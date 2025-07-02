b684204c0a528e9e486250ee303d8a568e8f53c9
Mantas9 <dev.mantasgr@gmail.com>
2025-07-02 14:28:29 +0300

    Fixed typos in help message, added original task PDF, created a readme file

--- constants/constants.go
+++ constants/constants.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-const helpMessage = `Usage: ./listings <parameters> <collection1> <collection2> ... <collectionX>` + "\nPossible parameters:\n\t--limit <intager>\tSets a limit to the amount of listings to fetch for each collection\n\t--min-price <number>\tFilters listings with a minimum price\n\t--max-price <number>\tFilters listings with a maximum price\n\t--desc\t\t\tCollection listings are automatically sorted by price ascendingly. This flag sorts the results descendingly"
+const helpMessage = `Usage: ./listings <parameters> <collection1> <collection2> ... <collectionX>` + "\nPossible parameters:\n\t--limit <integer>\tSets a limit to the amount of listings to fetch for each collection\n\t--min-price <number>\tFilters listings with a minimum price\n\t--max-price <number>\tFilters listings with a maximum price\n\t--desc\t\t\tSort by price in Descending order (default - by price in Ascending order)\n\t--json\t Export data in JSON format"
 
 // Prints a help message to terminal and exits the application
 func HelpMessage() {
